11411a270108fdbaa125eabc02825648508a34b7
Niko Matsakis <niko@alum.mit.edu>
2014-05-19 08:51:41 -0400

    WIP -- start new pipeline

---
+++ src/pipeline.js
@@ -0,0 +1,16 @@
+import { } from "./type.js";
+
+export function fn(f) {
+  var b = new Builder();
+}
+
+export function (f) {
+  var b = new Builder();
+}
+
+export class Builder {
+  constructor() {
+  }
+}
+
+export class 
